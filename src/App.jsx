7440ce8fc2bc9d506923c7365a1521b95a6a01e3
Moshood Sanusi <snsmoshood@gmail.com>
2023-08-21 00:27:29 +0100

    metame

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const [loading, setLoading] = useState(true)
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 10000);
+    }, 17000);
   });
   
 
@@ -19,96 +19,19 @@ const [loading, setLoading] = useState(true)
 {loading ? (
   <Loading />
 ) : (
-  <div>
-      
-  <div className='loader_bg'>
-      <div className='hexagon hexx' id='hexagon'></div>
-      <div className='hexagon hex1' id='hexagon'></div>
-      <div className='hexagon hex2' id='hexagon'></div>
-      <div className='hexagon hex3' id='hexagon'></div>
-      <div className='hexagon hex4' id='hexagon'></div>
-      <div className='hexagon hex5' id='hexagon'></div>
-      <div className='hexagon hex6' id='hexagon'></div> 
-      <div className='load_text' id='text'>
-        <h1 className='loh1'>It's a Meegos world, </h1>
-        <h1 className='loh2'>we are just living in it. </h1>
-        <div className='images'>
-        <div className="img1" id="img1">
-        
-        </div>
-        <div className="img2" id="img2">
-  
-        </div>
-        <div className="img3" id="img3">
-  
-        </div>
-        <div className="img4" id="img4">
-  
-        </div>
-        </div>
-      </div>
-
-      <div className='abhead '>
-              <h1 className='abhead jayboy'>Created by <span> Jayboy </span> for    <span className='meellist'> Meelist</span></h1>
-          
-          </div>
-    </div>
-{/* 
-    //About */}
-
-    
-<div className='about'>
-            <div className='hexagon' id='hexagon'></div>
-      <div className='hexagon hex1' id='hexagon'></div>
-      <div className='hexagon hex2' id='hexagon'></div>
-      <div className='hexagon hex3' id='hexagon'></div>
-      <div className='hexagon hex4' id='hexagon'></div>
-      <div className='hexagon hex5' id='hexagon'></div>
-      <div className='hexagon hex6' id='hexagon'></div> 
-        <div className='about_cont'>
-          <div className='abhead'>
-              <h1 className='abhead'>Coming Soon to </h1>
-            <span>Solana</span>
-          </div>
-          
-
-            <div className='abtcent'>
-            <div className="centhead">
-<h1>10000 </h1>
-                <span>TOTAL SUPPLY</span>
-</div >
-<div className="centinfo">
-  <div>
-    <h3>4800</h3>
-    <span>whitelist mint</span>
+<div>
+<div className='home'>
+ <div className="cont">
+  <div className="home_text">
+ <h1> Welcome to <span>Metame</span></h1>
+ <span>🎴 Card holders only 🎴</span>
   </div>
-  <div>
-    <h3>4444</h3>
-    <span>reserved for smyths</span>
-  </div>
-  <div>
-    <h3>500</h3>
-    <span>public mint</span>
-  </div>
-  <div>
-    <h3>256</h3>
-    <span>treasury/team/marketing</span>
-  </div>
+ </div>
 </div>
-            </div>
-
-            <div className="mint">
-              <span>Mint price and date to be released soon.</span> 
-            </div>
-
-            <div className='abhead '>
-              <h1 className='abhead jayboy'>Created by <span> Jayboy </span> for    <span className='meellist'> Meelist</span></h1>
-          
-          </div>
-        </div>
-    </div>
-
-    </div>
+<div className="about">
+  <h2>Metame is more than just an NFT project. It's a movement that challenges the conventional wisdom about Solana NFTs. We're here to prove that the bull market is far from over and that innovation is alive. Our unique approach to anime-themed generative NFTs sets us apart, and our passionate community is proof of that.</h2>
+</div>
+</div>
 )}
 </>
   )
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,13 +1,95 @@
-
+import { gsap } from "gsap";
+import { useEffect } from "react";
 
 const Loading = () => {
+
+  useEffect(() => {
+    const tl = gsap.timeline()
+
+    tl.to(
+      '.text', 
+      {
+        opacity: 0,
+        ease: 'power1.out',
+        delay: 2,
+        duration: 1
+      }
+    )
+    .to('.load_img', {
+      
+      opacity:1,
+      duration: 0.001,
+      
+      ease: 'power2.in',
+    })
+    .to(
+      '#img3', {
+        opacity:0,
+        ease: 'power1.out',
+        delay: 2,
+      }
+    )
+    .to(
+      '#img2', {
+        opacity:0,
+        ease: 'power1.out',
+        delay: 2,
+      }
+    )
+    .to(
+      '#img1', {
+        opacity:0,
+        ease: 'power1.out',
+        delay: 2,
+      }
+    )
+    .to(
+      '.text', 
+      {
+        opacity: 1,
+        ease: 'power1.in',
+        duration: 1
+      }
+    )
+    .to(
+      '.center', 
+      {
+        opacity: 0,
+        ease: 'power3.out',
+        duration: 1,
+        delay:2
+      }
+    )
+    .to(
+      '.cent', {
+        opacity:1,
+        ease: 'power3.out',
+        duration: 1,
+      }
+    )
+  }, [])
  
 
     
     
 
     return (
-  <div className="home"> 
+  <div className="loading"> 
+<div className="center">
+  <div className="text" id="text">
+    <h1>ME</h1>
+    <h1>TA</h1>
+    <h1>ME</h1>
+  </div>
+
+<img src="m1.jpg" alt="" className="load_img " id="img1"/>
+<img src="m8.jpg" alt="" className="load_img " id="img2"/>
+<img src="m7.jpg" alt="" className="load_img " id="img3"/>
+</div>
+
+<div className="cent">
+  <h1>Coming soon to <span>Solana</span></h1>
+</div>
   </div>
     );
   };
